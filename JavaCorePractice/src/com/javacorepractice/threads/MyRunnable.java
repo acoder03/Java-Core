a07d104591aba1b50d708745648d129c0ac11cac
acoder03 <acoder03@gmail.com>
2021-12-13 21:57:33 +0530

    thread some changes

--- JavaCorePractice/src/com/javacorepractice/threads/MyRunnable.java
+++ JavaCorePractice/src/com/javacorepractice/threads/MyRunnable.java
@@ -1,11 +1,31 @@
 package com.javacorepractice.threads;
-
+/*
+ * 
+ * Thread.currentThread()
+ * 
+ * 
+ * 
+ * 
+ */
 public class MyRunnable implements Runnable {
 
 	public void run() {
 		System.out.println(Thread.currentThread().getName());
-		
+		Thread.currentThread().setName("anji Thread"); // main thread name change
+		System.out.println(Thread.currentThread().getName());
 		System.out.println("aayush");
+		System.out.println(10/0);
 
+	}
+}
+
+class test1
+{
+	public static void main (String args[])
+	{
+		MyRunnable r = new MyRunnable();
+		
+		Thread t= new Thread(r);
+		t.start();
 	}
 }
--- JavaCorePractice/src/com/javacorepractice/threads/MyThread.java
+++ JavaCorePractice/src/com/javacorepractice/threads/MyThread.java
@@ -1,10 +1,53 @@
 package com.javacorepractice.threads;
 
+/* two ways to make threads 
+ * 1. by extending Thread class - it contains start() method and Thread class also 
+ * 		 implements Runnable interface
+ * 2. by implements Runnable interface -- it does not have start() method so indirectly 
+ * we have to call Thread class and pass instance of Runnable type to it 
+ * 3. run() method must be overridden because it have empty implementation in super classes
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
 public class MyThread extends Thread {
 
-	public void run() {
+	public void run() { // t.start() always call no-args run method
 
 		System.out.println("child class");
 	}
+	//overloading of run method
+	public void run(int i)
+	{
+		System.out.println("this is overloaded run method"+i);
+		
+	}
+	//overriding of start method
+//	public void start()
+//	{
+//		
+//	}
 
+}
+class Test2
+{
+	public static void main (String args[])
+	{
+		MyThread t = new MyThread() ;
+		
+		t.start();
+		
+		System.out.println("main thread");
+	}
 }
